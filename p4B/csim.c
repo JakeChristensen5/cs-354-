8b1619d4032af2aaa3d7992393e43ebe95df96f8
jake <83853752+JakeChristensen5@users.noreply.github.com>
2024-05-08 03:47:18 -0500

    Update csim.c

--- p4B/csim.c
+++ p4B/csim.c
@@ -1,34 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-// Main File:        csim.c
-// Purpose: A cache simulator program.
-// This File:        csim.c
-// Other Files:      N/A
-// Semester:         CS 354 Lecture 001 Fall 2022
-// Instructor:       deppeler
-//
-// Author:           Jake Christensen
-// Email:            Jrchristens2@wisc.edu
-// CS Login:         jakec
-//
-/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
-//                   fully acknowledge and credit all sources of help,
-//                   other than Instructors and TAs.
-//
-// Persons:          Identify persons by name, relationship to you, and email.
-//                   Describe in detail the the ideas and help they provided.
-//
-// Online sources:   avoid web searches to solve your problems, but if you do
-//                   search, be sure to include Web URLs and description of
-//                   of any information you find.
-//////////////////////////// 80 columns wide ///////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright 2013,2019-2020
-// Posting or sharing this file is prohibited, including any changes/additions.
-// Used by permission for Fall 2022
-//
-////////////////////////////////////////////////////////////////////////////////
-
 /*
  * csim.c:
  * A cache simulator that can replay traces (from Valgrind) and output
